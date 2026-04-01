f3e63f123664bbac01340c4ac522a4bc99ad2ed7
Varad Kottawar <87765623+Varad02@users.noreply.github.com>
2026-04-01 14:22:25 -0700

    Add: jump-game-ii - submission-5

---
+++ Data Structures & Algorithms/jump-game-ii/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [float('inf')] * n
+        dp[n - 1] = 0  # base case: already at end
+
+        for i in range(n - 2, -1, -1):
+            for j in range(1, nums[i] + 1):
+                if i + j < n:
+                    dp[i] = min(dp[i], 1 + dp[i + j])
+
+        return int(dp[0])
