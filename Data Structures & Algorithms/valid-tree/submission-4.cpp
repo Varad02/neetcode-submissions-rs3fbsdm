a255f5335acabe7183fd20077a7b892fe513abc7
Varad Kottawar <87765623+Varad02@users.noreply.github.com>
2026-07-22 15:46:40 -0700

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+        for (auto i : edges) {
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+        }
+        vector<int> vis(n, 0);
+        queue<pair<int, int>> q;
+        q.push({0, -1});
+        vis[0] = 1;
+        while (!q.empty()) {
+            auto [c, p] = q.front();
+            q.pop();
+            for (int i : adj[c]) {
+                if (p == i) continue;
+                if (vis[i]) return false;
+                q.push({i, c});
+                vis[i] = 1;
+            }
+        }
+        for (int i : vis)
+            if (!i) return false;
+        return true;
+    }
+};
