dd1cfe168ae00184dcb2dbfc76b0869c1da9e872
Varad Kottawar <87765623+Varad02@users.noreply.github.com>
2026-07-28 13:49:24 -0700

    Add: reconstruct-flight-path - submission-0

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+   public:
+    unordered_map<string, priority_queue<string, vector<string>, greater<string>>> graph;
+    vector<string> itinerary;
+
+    void dfs(string airport) {
+        while (!graph[airport].empty()) {
+            string next = graph[airport].top();
+            graph[airport].pop();
+            dfs(next);
+        }
+        itinerary.push_back(airport);
+    }
+
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        for (auto& ticket : tickets) {
+            graph[ticket[0]].push(ticket[1]);
+        }
+        dfs("JFK");
+        reverse(itinerary.begin(), itinerary.end());
+        return itinerary;
+    }
+};
