41f7745d55d0335c5eaf38e768428525ed16aad7
Varad Kottawar <87765623+Varad02@users.noreply.github.com>
2026-08-11 14:01:22 -0700

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(stones.begin(), stones.end());
+        while (pq.size() > 1) {
+            int stone1 = pq.top();
+            pq.pop();
+            int stone2 = pq.top();
+            pq.pop();
+            if (stone1 == stone2) continue;
+            pq.push(abs(stone1 - stone2));
+        }
+        return pq.size() == 0 ? 0 : pq.top();
+    }
+};
