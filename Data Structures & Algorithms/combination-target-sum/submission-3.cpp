48b7f8d740f516a1f7c1ebd99a110a492dfe489e
Varad Kottawar <87765623+Varad02@users.noreply.github.com>
2026-08-19 21:58:03 -0700

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int> > res;
+        vector<int> combination;
+        combinationSum(candidates, target, res, combination, 0);
+        return res;
+    }
+private:
+    void combinationSum(vector<int> &candidates, int target, vector<vector<int> > &res, vector<int> &combination, int i) {
+        if (!target) {
+            res.push_back(combination);
+            return;
+        }
+        for (; i != candidates.size() && target >= candidates[i]; i++) {
+            combination.push_back(candidates[i]);
+            combinationSum(candidates, target - candidates[i], res, combination, i);
+            combination.pop_back();
+        }
+    }
+};
